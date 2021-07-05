b3af44454cd284f267b0c0d00ebad60267d5f38f
fabidick22 <fabidick22@hotmail.es>
2021-07-05 16:48:31 -0500

    feat: restrict AfterStep function

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -3,7 +3,7 @@ void before(){
     println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
 }
 
-@AfterStep
+@AfterStep({ hookContext.step in config.afterSteps })
 void after(){
     println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
 }
