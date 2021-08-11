0651ce8cd5f46ab008f9b53b22f87b36f15c119a
fabidick22 <fabidick22@hotmail.es>
2021-08-11 14:20:15 -0500

    feat: add custom_command attri

--- libraries/detect_secrets/steps/detect_secrets_code.groovy
+++ libraries/detect_secrets/steps/detect_secrets_code.groovy
@@ -1,10 +1,12 @@
 void call(){
     stage("Yelp/detect-secrets: Detect secrets in the code"){
         String command = config.command?:"detect-secrets"
+        String args = config.args?:""
+        String custom_command = config.custom_command
         String pathToScan = config.path_to_scan?:"."
         String mode = config.detect_mode?:"scan"
         String image_tag = config.image_tag ?: "latest"
-        String detect_secrets_img = config.image_name ?: "lirantal/detect-secrets"
+        String detect_secrets_img = config.image_name ?: "detect-secrets"
         if(config.containsKey("enable_owasp")){
             inside_sdp_image "owasp-dep-check:$image_tag", {
                 unstash "workspace"
@@ -15,10 +17,11 @@ void call(){
                 }
             }
         }else{
+            String cmd = custom_command ? custom_command : "${command} ${mode} ${args} ${pathToScan}"
             inside_sdp_image "$detect_secrets_img:$image_tag", {
                 unstash "workspace"
                 try {
-                    sh "${command} ${mode} ${args} ${pathToScan}"
+                    sh cmd
                 }catch (ex) {
                     error "Error occured when running Detect secrets: ${ex.getMessage()}"
                 }
