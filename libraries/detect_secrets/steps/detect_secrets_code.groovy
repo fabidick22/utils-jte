067474e20bdc8f4f63f7fcf0c8271bbf614ad089
fabidick22 <fabidick22@hotmail.es>
2021-08-11 09:47:31 -0500

    feat: Add owasp imagen to test purpose

--- libraries/detect_secrets/steps/detect_secrets_code.groovy
+++ libraries/detect_secrets/steps/detect_secrets_code.groovy
@@ -4,14 +4,27 @@ void call(){
         String pathToScan = config.path_to_scan?:"."
         String mode = config.detect_mode?:"scan"
         String image_tag = config.image_tag ?: "latest"
-        inside_sdp_image "lirantal/detect-secrets:$image_tag" {
-            unstash "workspace"
-            try {
-                sh "${command} ${mode} ${args} ${pathToScan}"
-            }catch (ex) {
-                error "Error occured when running Detect secrets: ${ex.getMessage()}"
+        String detect_secrets_img = config.image_name ?: "lirantal/detect-secrets"
+        if(config.containsKey("enable_owasp")){
+            inside_sdp_image "owasp-dep-check:$image_tag" {
+                unstash "workspace"
+                try {
+                    sh "dependency-check -h"
+                }catch (ex) {
+                    error "Error occured when running Detect secrets: ${ex.getMessage()}"
+                }
+            }
+        }else{
+            inside_sdp_image "$detect_secrets_img:$image_tag" {
+                unstash "workspace"
+                try {
+                    sh "${command} ${mode} ${args} ${pathToScan}"
+                }catch (ex) {
+                    error "Error occured when running Detect secrets: ${ex.getMessage()}"
+                }
             }
         }
+
         println "scanning"
     }
 }
